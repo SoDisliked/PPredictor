a3ebd1d54593bd951218abfe53adf75777a16435
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2022-12-20 01:08:00 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './financial-vizualization.js'
+
+ReactDOM.render(
+    <React.StrictMode>
+
+    </React.StrictMode>
+    document.getElementById('root')
+)
