bad8b25f264c950cfbc5a38df16a74a28859e9dd
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-08 12:48:10 +0100

    Add files via upload

---
+++ Interpolate Prices.py
@@ -0,0 +1,30 @@
+'''
+Main file.
+'''
+import os 
+import sys 
+import pandas as pd 
+
+from interpolation import interpolate 
+from normalization import normalize 
+
+def main(dir_path, output_dir):
+    '''
+    Run PipeLine process.
+    '''
+    files = os.listdir(dir_path)
+
+    for file_name in files:
+        file_dataframe = pd.reas_csv(os.path.join(dir_path, file_name))
+
+        cols = ['high', 'low', 'open', 'close', 'volume', 'duration']
+
+        file_dataframe = interpolate(file_dataframe, cols)
+        file_dataframe = normalize(file_dataframe, cols)
+
+        file_dataframe.to_csv(
+            os.path.join(output_dir, file_name), encoding='utf-8'
+        )
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
