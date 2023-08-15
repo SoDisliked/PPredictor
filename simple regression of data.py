ee53e981a22460ce549caa1f3cc793c2d9ddff14
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-08-15 13:31:58 +0300

    Add files via upload

---
+++ simple regression of data.py
@@ -0,0 +1,30 @@
+import numpy as np 
+from training_model import train_model, get_model_metrics
+
+def test_train_model():
+    X_train = np.array([1,2,3,4,5,6]).reshape(-1, 1)
+    y_train = np.array([10, 9, 8, 7, 6, 5])
+    data = {"train": {"X": X_train, "y": y_train}}
+
+    reg_model = train_model(data, {"alpha": 0.01})
+
+    preds = reg_model.predict([[1], [2]])
+    np.testing.assert_almost_equal(preds, [9.99999999, 9.03041040])
+
+def test_get_model_metrics():
+
+    class MockModel:
+
+        @staticmethod
+        def predict(data):
+            return ([8.12545164, 7.1212121])
+        
+        X_test = np.array([3,4]).reshape(-1, 1)
+        y_test = np.array([8, 7])
+        data = {"test": {"X": X_test, "y": y_test}}
+
+        metrics = get_model_metrics(MockModel(), data)
+
+        assert 'mse' in metrics 
+        mse = metrics['mse']
+        np.testing.assert_almost_equal(mse, 0.7215544656452)
